6e47f346768799303889de697ce6d3a40ff57fc2
burakozbudak <burakozbudak25@hacettepe.edu.tr>
2025-07-29 05:13:25 +0300

    App.jsx güncellemesi

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import { useState } from "react";
 import { Route } from "react-router-dom";
 import ProductContextProvider from "./contexts/ProductContextProvider";
