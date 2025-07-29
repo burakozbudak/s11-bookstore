567e39eb5e3ff39cace8d4d83d88827011770e86
burakozbudak <burakozbudak25@hacettepe.edu.tr>
2025-07-29 05:18:33 +0300

    CartContextProvider.jsx
    bu dosya içine cart statei, addItem ve removeItem komutları tanımlandı.
    daha öncesinde bu dosya oluşturulmadan app dosyasında sarmalanmıştı.

---
+++ src/context/CartContextProvider.jsx
@@ -0,0 +1,23 @@
+import { createContext, useState } from "react";
+
+export const CartContext = createContext();
+
+const CartContextProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  const addItem = (item) => {
+    setCart((prevCart) => [...prevCart, item]);
+  };
+
+  const removeItem = (id) => {
+    setCart((prevCart) => prevCart.filter((item) => item.id !== id));
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addItem, removeItem }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartContextProvider;
